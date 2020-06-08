82b62bbbcd8c3e7c18424342e498c93a0a1ec668
Damião Junior <52061729+ajdamiao@users.noreply.github.com>
2020-06-08 11:00:33 -0300

    Adicionado Classe Executavel

---
+++ src/com/company/Executavel.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Executavel {
+
+    Banco bancoDados = new Banco();
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
         //(obs: no objeto conta poupança, faça um saque maior que o saldo atual).
         //Crie um objeto relatório e execute o método gerar relatório para cada conta criada.
 
+
     }
 }
